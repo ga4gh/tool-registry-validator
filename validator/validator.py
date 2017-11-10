3eddc194822fdbf26962b59d5709b5d4ab8a2d3c
garyluu <garylau.work@gmail.com>
2017-11-10 10:49:11 -0500

    Clean up

--- validator/validator.py
+++ validator/validator.py
@@ -1,9 +1,8 @@
 from io import BytesIO
 from subprocess import Popen, PIPE
-
 import requests
 from flask import Flask, send_file, request
-
+# import ga4gh_tool_registry.validate as validate
 from badge import passing_badge, failing_badge, warning_badge, error_badge
 
 app = Flask(__name__)
@@ -15,10 +14,14 @@ def hello_world():
 
 
 def _compute_badge(url):
-    process = Popen(['ga4gh-tool-registry-validate ga4gh-tool-discovery.yaml annotations.yml ' + url], stdout=PIPE, stderr=PIPE, shell=True)
+    # validate.validate('ga4gh-tool-discovery.yaml', 'annotations.yml', url, False, False, False)
+    commandargs = ['ga4gh-tool-registry-validate', 'ga4gh-tool-discovery.yaml', 'annotations.yml', url]
+    process = Popen(commandargs, stdout=PIPE, stderr=PIPE)
     (out, err) = process.communicate()
-    if err != 'API returned valid response\n':
+    if "Failed to establish a new connection" in err:
         return error_badge()
+    if err != 'API returned valid response\n':
+        return failing_badge()
     else:
         if url == 'failing':
             return failing_badge()
@@ -34,5 +37,6 @@ def status_badge():
     r = requests.get(_compute_badge(url))
     return send_file(BytesIO(r.content), mimetype=r.headers['Content-Type'])
 
-if __name__== '__main__':
-    app.run(debug=True,host='0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

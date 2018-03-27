97171284be16e058a05b4dbbc8d85085e1768b07
garyluu <garylau.work@gmail.com>
2018-03-27 16:49:35 -0400

    Change branch for ga4gh.yaml

--- validator/constants.py
+++ validator/constants.py
@@ -3,5 +3,5 @@ GENERATED_SWAGGER = 'GA4GH.swagger.json'
 PREPROCESSED_SWAGGER = 'ga4gh-tool-discovery.yaml'
 EXPECTED_PASSING_TESTS = 9
 GITHUB_BASEURL = 'https://raw.githubusercontent.com/ga4gh/tool-registry-schemas'
-GITHUB_BRANCH = 'feature/trsv_changes'
+GITHUB_BRANCH = 'develop'
 GITHUB_FILE_PATH = 'src/main/resources/swagger/ga4gh-tool-discovery.yaml'

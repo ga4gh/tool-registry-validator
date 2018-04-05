647e42b95735d6debcfce53697cae1f4a450ded0
garyluu <garylau.work@gmail.com>
2018-04-05 09:41:43 -0400

    Update increased amount of endpoints

--- validator/constants.py
+++ validator/constants.py
@@ -1,7 +1,7 @@
 SWAGGER = "processed-ga4gh-tool-discovery.yaml"
 GENERATED_SWAGGER = 'GA4GH.swagger.json'
 PREPROCESSED_SWAGGER = 'ga4gh-tool-discovery.yaml'
-EXPECTED_PASSING_TESTS = 9
+EXPECTED_PASSING_TESTS = 11
 GITHUB_BASEURL = 'https://raw.githubusercontent.com/ga4gh/tool-registry-schemas'
 GITHUB_BRANCH = 'develop'
 GITHUB_FILE_PATH = 'src/main/resources/swagger/ga4gh-tool-discovery.yaml'

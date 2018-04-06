29476ec62d5df6353fe16f56a5adb9f0a4503d40
garyluu <garylau.work@gmail.com>
2018-04-06 10:10:07 -0400

    Fix

--- validator/constants.py
+++ validator/constants.py
@@ -3,5 +3,5 @@ GENERATED_SWAGGER = 'GA4GH.swagger.json'
 PREPROCESSED_SWAGGER = 'ga4gh-tool-discovery.yaml'
 EXPECTED_PASSING_TESTS = 11
 GITHUB_BASEURL = 'https://raw.githubusercontent.com/ga4gh/tool-registry-schemas'
-GITHUB_BRANCH = 'c606590546e0a4c7d57f918a8a780cd1ff8e3656'
+GITHUB_BRANCH = 'bdede80ab952fd403249d780ec1d104723c440d2'
 GITHUB_FILE_PATH = 'src/main/resources/swagger/ga4gh-tool-discovery.yaml'

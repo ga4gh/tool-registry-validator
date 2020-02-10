e626c6cb0265533ddeea8b5c99e47e11b8b456bd
garyluu <garylau.work@gmail.com>
2020-02-10 15:04:18 -0500

    Update release

--- validator/constants.py
+++ validator/constants.py
@@ -3,5 +3,5 @@ GENERATED_SWAGGER = 'GA4GH.swagger.json'
 PREPROCESSED_SWAGGER = 'ga4gh-tool-discovery.yaml'
 EXPECTED_PASSING_TESTS = 11
 GITHUB_BASEURL = 'https://raw.githubusercontent.com/ga4gh/tool-registry-schemas'
-GITHUB_BRANCH = '2.0.0-beta.2'
+GITHUB_BRANCH = '2.0.0'
 GITHUB_FILE_PATH = 'src/main/resources/swagger/ga4gh-tool-discovery.yaml'

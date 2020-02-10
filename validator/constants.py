3d00c62a1d2f93d491d545efc7442f0bbb650981
garyluu <garylau.work@gmail.com>
2020-02-10 15:07:57 -0500

    Update swagger location

--- validator/constants.py
+++ validator/constants.py
@@ -4,4 +4,4 @@ PREPROCESSED_SWAGGER = 'ga4gh-tool-discovery.yaml'
 EXPECTED_PASSING_TESTS = 11
 GITHUB_BASEURL = 'https://raw.githubusercontent.com/ga4gh/tool-registry-schemas'
 GITHUB_BRANCH = '2.0.0'
-GITHUB_FILE_PATH = 'src/main/resources/swagger/ga4gh-tool-discovery.yaml'
+GITHUB_FILE_PATH = 'openapi/ga4gh-tool-discovery.yaml'
